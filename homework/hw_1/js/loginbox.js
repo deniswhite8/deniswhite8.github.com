997ef72a79d1f9ffdf3e8132db5f5a03149c6bcc
denis <deniswhite8@gmail.com>
2013-05-25 18:29:25 +0400

    blablabla

--- homework/hw_1/js/loginbox.js
+++ homework/hw_1/js/loginbox.js
@@ -1,12 +1,13 @@
 $(document).ready(function() {
 	
 	$('#login').on('click', function() {
-		$('#overlay').show();
-		$('#loginBox').show();
-		$('#loginBox').css({'opacity': 0, 'margin-top':-215}).animate({'opacity':1, 'margin-top':-115}, 1000);
 		$('#main').addClass('blur');
 		$('#header').addClass('blur');
 		$('#footer').addClass('blur');
+		$('#overlay').show();
+		$('#loginBox').show();
+		$('#loginBox').css({'opacity': 0, 'margin-top':-215}).animate({'opacity':1, 'margin-top':-115}, 1000);
+
 	});
 
 	$('#cancel').on('click', function() {
